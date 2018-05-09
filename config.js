a03504b5ced7780ba7d92fb1a9f29fce5f628a2c
zero <f12998765@yeah.net>
2018-05-10 03:23:45 +0800

    修改config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 var config=new Map([
-    ["img_url","https://raw.githubusercontent.com/f12998765/xizero-wiki/master/"],
+    ["img_url","https://raw.githubusercontent.com/f12998765/wiki/master/"],
 ]);
